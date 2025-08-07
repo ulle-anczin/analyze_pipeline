836f2cb42ec86ba77437b3ac1af7602cd73374f1
ulle-anczin <ulle-anczin@users.noreply.github.com>
2025-08-07 23:11:17 +0000

    fix: resolve syntax error in ${file}

---
+++ main.py
@@ -0,0 +1,16 @@
+package analyze_pipeline
+
+import java.io.File
+
+fun main() {
+    val fileName = "main"
+    println("Processing ${fileName} in analyze_pipeline")
+
+    val file = File(fileName)
+    if (file.exists()) {
+        val lines = file.readLines()
+        println("Read ${lines.size} lines")
+    } else {
+        println("File does not exist")
+    }
+}
